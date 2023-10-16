f5d74c0bfb911ba19075bfa4fb76d70869b5b183
Sharjeel Faiq <mrmalik610@gmail.com>
2023-10-16 15:21:15 +0500

    Loading animation updated with added functionalities and new styling

---
+++ main.js
@@ -0,0 +1,122 @@
+function set_size(width_param) {
+    const loader = document.getElementById("loader");
+    const loader_background = document.getElementById("loader-background");
+    if (width_param === 50) {
+        loader.style.width = 50 + "px";
+        loader.style.height = 50 + "px";
+        loader.style.borderWidth = "2px";
+        loader_background.style.width = 050 + "px";
+        loader_background.style.height = 050 + "px";
+    }
+    else if (width_param === 150) {
+        loader.style.width = 150 + "px";
+        loader.style.height = 150 + "px";
+        loader.style.borderWidth = "4px";
+        loader_background.style.width = 150 + "px";
+        loader_background.style.height = 150 + "px";
+    }
+    else if (width_param === 250) {
+        loader.style.width = 250 + "px";
+        loader.style.height = 250 + "px";
+        loader.style.borderWidth = "6px";
+        loader_background.style.width = 250 + "px";
+        loader_background.style.height = 250 + "px";
+    }
+    else if (width_param === 350) {
+        loader.style.width = 350 + "px";
+        loader.style.height = 350 + "px";
+        loader.style.borderWidth = "8px";
+        loader_background.style.width = 350 + "px";
+        loader_background.style.height = 350 + "px";
+    }
+    else if (width_param === 450) {
+        loader.style.width = 450 + "px";
+        loader.style.height = 450 + "px";
+        loader.style.borderWidth = "10px";
+        loader_background.style.width = 450 + "px";
+        loader_background.style.height = 450 + "px";
+    }
+    else if (width_param === 550) {
+        loader.style.width = 550 + "px";
+        loader.style.height = 550 + "px";
+        loader.style.borderWidth = "12px";
+        loader_background.style.width = 550 + "px";
+        loader_background.style.height = 550 + "px";
+    }
+}
+
+function increase_size() {
+    const loader = document.getElementById("loader");
+    const loader_background = document.getElementById("loader-background");
+
+    let loader_style = window.getComputedStyle(loader);
+    let loader_background_style = window.getComputedStyle(loader);
+
+    let loader_width = Number(loader_style.width.substring(0, 2)) >= 50 ? Number(loader_style.width.substring(0, 2)) : Number(loader_style.width.substring(0, 3));
+    let loader_height = Number(loader_style.height.substring(0, 2)) >= 50 ? Number(loader_style.height.substring(0, 2)) : Number(loader_style.height.substring(0, 3));
+    let loader_background_width = Number(loader_background_style.width.substring(0, 2)) >= 50 ? Number(loader_background_style.width.substring(0, 2)) : Number(loader_background_style.width.substring(0, 3));
+    let loader_background_height = Number(loader_background_style.height.substring(0, 2)) >= 50 ? Number(loader_background_style.height.substring(0, 2)) : Number(loader_background_style.height.substring(0, 3));
+
+    loader.style.width = loader_width + 5 + "px";
+    loader.style.height = loader_height + 5 + "px";
+    loader_background.style.width = loader_background_width + 5 + "px";
+    loader_background.style.height = loader_background_height + 5 + "px";
+
+    if (loader_width < 150) {
+        loader.style.borderWidth = "2px";
+    } else if (loader_width < 250) {
+        loader.style.borderWidth = "4px";
+    } else if (loader_width < 350) {
+        loader.style.borderWidth = "6px";
+    } else if (loader_width < 450) {
+        loader.style.borderWidth = "8px";
+    } else if (loader_width < 550) {
+        loader.style.borderWidth = "10px";
+    } else if (loader_width >= 550) {
+        loader.style.borderWidth = "12px";
+    }
+}
+
+function decrease_size() {
+    const loader = document.getElementById("loader");
+    const loader_background = document.getElementById("loader-background");
+
+    let loader_style = window.getComputedStyle(loader);
+    let loader_background_style = window.getComputedStyle(loader);
+
+    let loader_width = Number(loader_style.width.substring(0, 2)) >= 50 ? Number(loader_style.width.substring(0, 2)) : Number(loader_style.width.substring(0, 3));
+    let loader_height = Number(loader_style.height.substring(0, 2)) >= 50 ? Number(loader_style.height.substring(0, 2)) : Number(loader_style.height.substring(0, 3));
+    let loader_background_width = Number(loader_background_style.width.substring(0, 2)) >= 50 ? Number(loader_background_style.width.substring(0, 2)) : Number(loader_background_style.width.substring(0, 3));
+    let loader_background_height = Number(loader_background_style.height.substring(0, 2)) >= 50 ? Number(loader_background_style.height.substring(0, 2)) : Number(loader_background_style.height.substring(0, 3));
+
+    loader.style.width = loader_width - 5 + "px";
+    loader.style.height = loader_height - 5 + "px";
+    loader_background.style.width = loader_background_width - 5 + "px";
+    loader_background.style.height = loader_background_height - 5 + "px";
+
+    if (loader_width < 150) {
+        loader.style.borderWidth = "2px";
+    } else if (loader_width < 250) {
+        loader.style.borderWidth = "4px";
+    } else if (loader_width < 350) {
+        loader.style.borderWidth = "6px";
+    } else if (loader_width < 450) {
+        loader.style.borderWidth = "8px";
+    } else if (loader_width < 550) {
+        loader.style.borderWidth = "10px";
+    } else if (loader_width >= 550) {
+        loader.style.borderWidth = "12px";
+    }
+}
+
+function clockwise() {
+    const clockwise_rotation = "loader 3s linear infinite";
+    const loader = document.getElementById("loader");
+    loader.style.animation = clockwise_rotation;
+}
+
+function anti_clockwise() {
+    const anti_clockwise_rotation = "loader 3s linear infinite reverse";
+    const loader = document.getElementById("loader");
+    loader.style.animation = anti_clockwise_rotation;
+}
